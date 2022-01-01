a1df7c06a7376baf76537033928cbc1c5c04c7b6
gpusystem <lionmini@naver.com>
2022-01-01 17:20:18 +0000

    split master to 2 files

--- setup_k8s_master_2.sh
+++ setup_k8s_master_2.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
 
-#------------- disable swap partition
-sudo swapoff -a
-echo "You must disable swap partition before installing k8s."
-read -p "Did you turn off swap? [yes/no] " answer
-if [ ${answer} = yes ] || [ ${answer} = y ] ; then
-	echo ""
-	else echo "disable swap first" && exit
-fi
-
-# static IP check
-read -p "Is the system's IP fixed for your stable k8s environment? (yes/no) " answer
-if [ ${answer} = yes ] || [ ${answer} = y ] ; then
-	echo ""
-	else echo "set IP static" && exit
-fi
-
-#------------- disable ufw
-sudo systemctl stop ufw
-sudo systemctl disable ufw
-
-#------------- install docker
-sudo apt-get remove docker docker-engine docker.io
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-sudo apt install -y docker-ce
-
 #-------------- make docker use systemd not cgroupfs
 echo "{" >> /etc/docker/daemon.json
 echo "  \"exec-opts\": [\"native.cgroupdriver=systemd\"]," >> /etc/docker/daemon.json
