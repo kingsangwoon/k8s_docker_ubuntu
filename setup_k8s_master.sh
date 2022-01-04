94da63fd7d7a72cb9d636737178edaecf37bfabf
JungWKim <lionmini@naver.com>
2022-01-05 00:05:32 +0900

    integrated all files into a file

---
+++ setup_k8s_master.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+#------------- disable swap partition
+swapoff -a
+echo "You must disable swap partition before installing k8s."
+read -p "Did you turn off swap? [yes/no] " answer
+if [ ${answer} = yes ] || [ ${answer} = y ] ; then
+        echo ""
+        else echo "disable swap first" && exit
+fi
+
+# static IP check
+read -p "Is the system's IP fixed for your stable k8s environment? (yes/no) " answer
+if [ ${answer} = yes ] || [ ${answer} = y ] ; then
+        echo ""
+        else echo "set IP static" && exit
+fi
+
+# static IP check
+read -p "Did you login as root? (yes/no) " answer
+if [ ${answer} = yes ] || [ ${answer} = y ] ; then
+        echo ""
+        else echo "press \"sudo su\" first!" && exit
+fi
+
+#------------- disable ufw
+systemctl stop ufw
+systemctl disable ufw
+
+#------------- install docker
+apt-get remove docker docker-engine docker.io
+apt update
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
+apt install -y docker-ce
+
+#-------------- make docker use systemd not cgroupfs
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+systemctl daemon-reload
+systemctl restart docker
+
+#------------- letting iptables see bridged traffic
+echo "br_netfilter" >> /etc/modules-load.d/k8s.conf
+echo "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.d/k8s.conf
+echo "net.bridge.bridge-nf-call-ip6tables = 1" >> /etc/sysctl.d/k8s.conf
+sysctl --system
+
+#------------- install kubeadm/kubelete/kubectl
+apt-get update
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
+#------------- create a cluster
+read -p "Enter the system's IP : " ip
+kubeadm init --control-plane-endpoint "${ip}:6443" --upload-certs --pod-network-cidr "10.244.0.0/16"
+
+#------------- allow current account to use kubectl without "sudo"
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+#------------- install CNI network addon
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
+#------------- enable kubectl & kubeadm auto-completion
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+echo "source <(kubeadm completion bash)" >> ~/.bashrc
+source ~/.bashrc
+
+#------------- enable ssh connection && open port 80
+apt install openssh-server
+ufw allow 22
+ufw allow 80
+
+#------------- allow specific ports for k8s
+ufw allow 6443
+ufw allow 2379
+ufw allow 2380
+ufw allow 10250
+ufw allow 10251
+ufw allow 10252
+systemctl enable ufw
+systemctl start ufw
+ufw enable
