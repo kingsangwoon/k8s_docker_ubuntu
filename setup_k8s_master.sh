1f1a6ca6448d0220777460dd252a187078d5de18
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2022-01-10 20:37:28 +0900

    Update setup_k8s_master.sh

--- setup_k8s_master.sh
+++ setup_k8s_master.sh
@@ -61,7 +61,6 @@ kubeadm init --control-plane-endpoint "${ip}:6443" --upload-certs --pod-network-
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
 
 #------------- install CNI network addon
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
