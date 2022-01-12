d379e22ebeacadfa8c1ca6cc6d2070a51bba8ac6
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2022-01-12 23:25:18 +0900

    Update setup_k8s_master.sh

--- setup_k8s_master.sh
+++ setup_k8s_master.sh
@@ -27,16 +27,16 @@ apt update
 apt install -y docker-ce
 
 #-------------- make docker use systemd not cgroupfs
-cat > /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
+#cat > /etc/docker/daemon.json <<EOF
+#{
+#  "exec-opts": ["native.cgroupdriver=systemd"],
+#  "log-driver": "json-file",
+#  "log-opts": {
+#    "max-size": "100m"
+#  },
+#  "storage-driver": "overlay2"
+#}
+#EOF
 
 mkdir -p /etc/systemd/system/docker.service.d
 systemctl daemon-reload
