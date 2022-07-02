7aa79f07da853e613d1f071db4e77263696ecca0
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2022-07-03 00:37:54 +0900

    Update setup_worker.sh

--- setup_worker.sh
+++ setup_worker.sh
@@ -19,13 +19,15 @@ systemctl stop ufw
 systemctl disable ufw
 
 #------------- install docker
-apt-get remove docker docker-engine docker.io
 apt update
-apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt install -y ca-certificates curl gnupg lsb-release
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
-apt install -y docker-ce
+apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 #-------------- make docker use systemd not cgroupfs
 cat > /etc/docker/daemon.json <<EOF
