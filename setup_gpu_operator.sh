263b5a5affecea06bf75883901194b4c456c30c4
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2022-08-06 22:38:41 +0900

    Create setup_gpu_operator.sh

---
+++ setup_gpu_operator.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# this will create 
+# (1) nvidia driver daemonset
+# (2) nvidia container toolkit daemonset
+# (3) nvidia cuda daemonset
+# (4) nvidia dcgm exporter
+# (5) nvidia operator feature discovery
+# (6) nvidia operator validator
+# (7) nvidia gpu feature discovery
+# (8) nvidia device plugin
+# (9) nvidia mig manager
+
+# add NVIDIA Helm repository
+helm repo add nvidia https://helm.ngc.nvidia.com/nvidia \
+  && helm repo update
+
+helm install --wait --generate-name \
+     -n gpu-operator --create-namespace \
+     nvidia/gpu-operator
