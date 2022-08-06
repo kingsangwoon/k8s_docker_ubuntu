0e75f571cfcc8ec926b1e2cdb8688e3c79d343f6
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2022-08-06 22:52:01 +0900

    Update setup_master.sh

--- setup_master.sh
+++ setup_master.sh
@@ -76,7 +76,7 @@ apt-get update
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
-apt-get install -y kubelet kubeadm kubectl
+apt-get install -y kubelet=1.20.11-00 kubeadm=1.20.11-00 kubectl=1.20.11-00
 apt-mark hold kubelet kubeadm kubectl
 
 #------------- create a cluster
